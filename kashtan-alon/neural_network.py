60664036667ec9be4a30a7f08f2941065c4c8777
Avery <averyzanegriffin@gmail.com>
2022-02-21 11:56:53 -0500

    Fixed a major bug in the L OR R loss function. Works now

--- kashtan-alon/neural_network.py
+++ kashtan-alon/neural_network.py
@@ -55,7 +55,7 @@ def calculate_loss(prediction, sample, gen_num, mvg, mvg_frequency):
         if sample["int_label"] == 3: label = 1
         else: label = 0
     elif not goal_is_and:
-        if sample["int_label"] == 1 or sample["int_label"] == 2: label = 1
+        if sample["int_label"] == 1 or sample["int_label"] == 2 or sample["int_label"] == 3: label = 1
         else: label = 0
 
     return int((prediction - label)**2)
