59223f2ec7c48ed2e92870e404e15d4dd4abad79
Avery Griffin <averyzanegriffin@gmail.com>
2022-02-24 04:59:00 -0500

    Code now visualizes communities detected by networkx's algorithm

--- kashtan-alon/network2graph.py
+++ kashtan-alon/network2graph.py
@@ -1,7 +1,9 @@
 import networkx as nx
+from networkx.algorithms.community.centrality import girvan_newman
 import matplotlib.pyplot as plt
 import numpy as np
 import json
+from data_viz import visualize_solo_network
 
 
 def the_hard_way(network):
@@ -26,41 +28,48 @@ def load_networks():
     return population
 
 
+population = load_networks()
+network = population[0]
+
 # Convert network to graph
-G = nx.DiGraph()
+G = nx.Graph()
 
 input = ["x1", "x2", "x3", "x4", "x5", "x6", "x7", "x8"]
-l1 = ["n1l1", "n2l1", "n3l1", "n4l1", "n5l1", "n6l1", "n7l1", "n8l1"]
-l2 = ["n1l2", "n2l2", "n3l2", "n4l2"]
-l3 = ["n1l3", "n2l3"]
+l1 = ["L1.1", "L1.2", "L1.3", "L1.4", "L1.5", "L1.6", "L1.7", "L1.8"]
+l2 = ["L2.1", "L2.2", "L2.3", "L2.4"]
+l3 = ["L3.1", "L3.2"]
 output = ["output"]
 
 # Adding input layer nodes and connections to 1st layer
 for i in range(len(input)):
-    G.add_nodes_from([(input[i], {'activation': 1, 'pos': (0, 50*(i+1))})])
-    for j in l1:
-        G.add_edges_from([(input[i], j, {'weight': np.random.randint(-2,3)})])
+    G.add_nodes_from([(input[i], {'activation': 1, 'pos': (0, 35*(i+1))})])
+    for j in range(len(l1)):
+        if network['thetas'][0][i][j] != 0:
+            G.add_edges_from([(input[i], l1[j], {'weight': network['thetas'][0][i][j] })])
 # Adding 1st layer nodes and connections to 2nd layer
 for k in range(len(l1)):
-    G.add_nodes_from([(l1[k], {'activation': 1, 'pos': (150, 50*(k+1))})])
-    for l in l2:
-        G.add_edges_from([(l1[k], l, {'weight': np.random.randint(-2,3)})])
+    G.add_nodes_from([(l1[k], {'activation': network['thresholds'][0][k], 'pos': (150, 35*(k+1))})])
+    for l in range(len(l2)):
+        if network['thetas'][1][k][l] != 0:
+            G.add_edges_from([(l1[k], l2[l], {'weight': network['thetas'][1][k][l] })])
 # Adding 2nd layer nodes and connections to 3rd layer
 for m in range(len(l2)):
-    G.add_nodes_from([(l2[m], {'activation': 1, 'pos': (300, 50*(m+3))})])
-    for n in l3:
-        G.add_edges_from([(l2[m], n, {'weight': np.random.randint(-2,3)})])
+    G.add_nodes_from([(l2[m], {'activation': network['thresholds'][1][m], 'pos': (300, 35*(m+3))})])
+    for n in range(len(l3)):
+        if network['thetas'][2][m][n] != 0:
+            G.add_edges_from([(l2[m], l3[n], {'weight': network['thetas'][2][m][n] })])
 # Adding 3rd layer nodes and connections to output layer
 for o in range(len(l3)):
-    G.add_nodes_from([(l3[o], {'activation': 1, 'pos': (450, 50*(o+4))})])
-    for p in output:
-        G.add_edges_from([(l3[o], p, {'weight': np.random.randint(-2,3)})])
+    G.add_nodes_from([(l3[o], {'activation': network['thresholds'][2][o], 'pos': (450, 35*(o+4))})])
+    for p in range(len(output)):
+        if network['thetas'][3][o][p] != 0:
+            G.add_edges_from([(l3[o], output[p], {'weight': network['thetas'][3][o][p] })])
 # Adding output layer nodes
-G.add_nodes_from([(output[0], {'activation': 1, 'pos': (550, 250)})])
+G.add_nodes_from([(output[0], {'activation': network['thresholds'][3][0], 'pos': (550, 160)})])
 
 # Mapping edge weights to colors
 edges = G.edges()
-edge_color_map = {-2: 'darkred', -1: 'r', 0: 'w', 1: 'c', 2: 'b'}
+edge_color_map = {-2: 'darkred', -1: 'r', 0: 'pink', 1: 'c', 2: 'b'}
 edge_colors = [edge_color_map[G[u][v]['weight']] for u,v in edges]
 
 # Defining the positions of each node
@@ -70,6 +79,38 @@ for n in G.nodes:
 
 stop = 1
 
-nx.draw(G, pos=node_pos, edge_color=edge_colors, with_labels=True)
-plt.show()
+# nx.draw(G, pos=node_pos, edge_color=edge_colors, node_size=1200, with_labels=True)
+# plt.show()
 
+communities = girvan_newman(G)
+node_groups = []
+for com in next(communities):
+    node_groups.append(list(com))
+
+print(node_groups)
+print(len(node_groups))
+
+module_color_map = ['orange', 'lime', 'k', 'r', 'yellow']
+module_colors = []
+for node in G.nodes:
+    for c in range(len(node_groups)):
+        if node in node_groups[c]:
+            module_colors.append(module_color_map[c])
+            break
+
+
+# nx.draw(G, pos=node_pos, edge_color=module_colors, node_size=1200, with_labels=True)
+# plt.show()
+
+fig = plt.figure(figsize=(16,8))
+ax1 = fig.add_subplot(1, 2, 1)
+ax2 = fig.add_subplot(1, 2, 2)
+nx.draw(G, pos=node_pos, edge_color=edge_colors, node_size=1200, with_labels=True, ax=ax1)
+nx.draw(G, pos=node_pos, edge_color=edge_colors, node_color=module_colors, node_size=1200, with_labels=True, ax=ax2)
+# ax1.set_title('Best Loss Each Generation')
+# ax1.legend()
+# ax2.set_xlabel('Generation (n)')
+# ax2.set_ylabel('Loss')
+# ax2.set_title('Average Loss Each Generation')
+# ax2.legend()
+plt.show()
