45e5f31ad9bfa39c03c49c6a097a5280669a3ea0
Avery Griffin <averyzanegriffin@gmail.com>
2022-02-24 18:02:02 -0500

    Code for making an explictely modular network from scratch

--- kashtan-alon/inspecting_networks.py
+++ kashtan-alon/inspecting_networks.py
@@ -11,59 +11,67 @@ def default(obj):
     raise TypeError('Not serializable')
 
 
-# theta1 = np.array([[-1,-1,1,0,0,0,0,0],
-#                    [-1,1,1,0,1,0,0,0],
-#                    [1,1,0,2,0,1,0,0],
-#                    [0,0,0,1,1,0,-1,0],
-#                    [0,0,0,0,0,0,0,-1],
-#                    [0,0,0,0,0,0,0,0],
-#                    [0,0,0,0,-1,0,0,0],
-#                    [0,0,-1,0,0,1,2,2]])
-# # theta1 = np.array([[2,1,0,0,0,0,0,0],
-# #                    [-1,0,1,-2,0,0,0,0],
-# #                    [0,1,0,0,1,0,0,0],
-# #                    [0,-1,-1,0,0,0,-1,-1],
-# #                    [0,0,0,1,1,0,0,0],
-# #                    [0,0,0,0,1,2,0,0],
-# #                    [0,0,0,0,0,1,0,-1],
-# #                    [0,0,0,0,0,0,1,-1]])
-# theta2 = np.array([[1,-2,0,0],
+theta1 = np.array([[-1,-1,1,1,0,0,0,0],
+                   [-1,0,1,0,0,0,0,0],
+                   [0,0,-2,2,0,0,0,0],
+                   [0,1,1,1,0,0,0,0],
+                   [0,0,0,0,0,0,1,-1],
+                   [0,0,0,0,1,1,0,0],
+                   [0,0,0,0,-1,0,2,1],
+                   [0,0,0,0,1,1,2,2]])
+# theta1 = np.array([[2,1,0,0,0,0,0,0],
+#                    [-1,0,1,-2,0,0,0,0],
+#                    [0,1,0,0,1,0,0,0],
+#                    [0,-1,-1,0,0,0,-1,-1],
+#                    [0,0,0,1,1,0,0,0],
+#                    [0,0,0,0,1,2,0,0],
+#                    [0,0,0,0,0,1,0,-1],
+#                    [0,0,0,0,0,0,1,-1]])
+theta2 = np.array([[1,-2,0,0],
+                   [2,0,0,0],
+                   [0,1,0,0],
+                   [-1,-2,0,0],
+                   [0,0,1,1],
+                   [0,0,1,0],
+                   [0,0,2,-1],
+                   [0,0,0,1]])
+# theta2 = np.array([[0,0,0,0],
+#                    [-1,1,0,0],
+#                    [-2,-1,0,0],
+#                    [0,0,2,0],
 #                    [0,0,0,0],
-#                    [-1,0,-2,0],
-#                    [0,0,0,-2],
-#                    [0,0,0,0],
-#                    [-1,0,0,0],
-#                    [0,0,0,0],
-#                    [0,-1,1,1]])
-# # theta2 = np.array([[0,0,0,0],
-# #                    [-1,1,0,0],
-# #                    [-2,-1,0,0],
-# #                    [0,0,2,0],
-# #                    [0,0,0,0],
-# #                    [0,1,-1,-1],
-# #                    [0,0,0,1],
-# #                    [0,0,0,1]])
-# theta3 = np.array([[2,0],
-#                    [0,1],
+#                    [0,1,-1,-1],
+#                    [0,0,0,1],
+#                    [0,0,0,1]])
+theta3 = np.array([[2,0],
+                   [1,0],
+                   [0,1],
+                   [0,-1]])
+# theta3 = np.array([[-2,0],
 #                    [0,0],
-#                    [1,-1]])
-# # theta3 = np.array([[-2,0],
-# #                    [0,0],
-# #                    [0,-2],
-# #                    [1,1]])
-# theta4 = np.array([[-1],
-#                    [1]])
-# # theta4 = np.array([[1],
-# #                    [-2]])
-# thrsh1 = np.array([[0],[-4],[-1],[0],[-4],[0],[-2],[3]])
-# thrsh2 = np.array([[-2],[-1],[1],[-3]])
-# thrsh3 = np.array([[1],[-4]])
-# thrsh4 = np.array([[0]])
-#
-# thetas = [theta1, theta2, theta3, theta4]
-# thresholds = [thrsh1, thrsh2, thrsh3, thrsh4]
-#
-# network = {"thetas": thetas, "thresholds": thresholds, "loss": 0}
+#                    [0,-2],
+#                    [1,1]])
+theta4 = np.array([[-1],
+                   [1]])
+# theta4 = np.array([[1],
+#                    [-2]])
+thrsh1 = np.array([[0],[-4],[-1],[0],[-4],[0],[-2],[3]])
+thrsh2 = np.array([[-2],[-1],[1],[-3]])
+thrsh3 = np.array([[1],[-4]])
+thrsh4 = np.array([[0]])
+
+thetas = [theta1, theta2, theta3, theta4]
+thresholds = [thrsh1, thrsh2, thrsh3, thrsh4]
+
+network = {"thetas": thetas, "thresholds": thresholds, "loss": 0}
+
+# Saved the weights at the very end
+w_file = open(f"saved_weights/modular_network.json", "w")
+json.dump(network, w_file, default=default)
+w_file.close()
+
+visualize_solo_network(network, 'modular_network')
+
 
 # samples = generate_samples(100)
 # for i in range(len(samples)):
@@ -102,19 +110,19 @@ def the_hard_way(network):
             if isinstance(network[key][i], list):
                 network[key][i] = np.array(network[key][i])
 
-population = []
-for i in range(100):
-    w_file = open(f"saved_weights/network_{i}.json", "r")
-    network = json.load(w_file)
-    the_hard_way(network)
-    population.append(network)
-    w_file.close()
-
-
-samples = load_samples(100, "samples")
-# for n in range(len(population)):
-#     visualize_solo_network(population[n], f"reconstructed_{n}")
-loss = evaluate_population(population, samples, 1, False)
-print("Loss: ", loss)
-
-check = 4
+# population = []
+# for i in range(100):
+#     w_file = open(f"saved_weights/network_{i}.json", "r")
+#     network = json.load(w_file)
+#     the_hard_way(network)
+#     population.append(network)
+#     w_file.close()
+#
+#
+# samples = load_samples(100, "samples")
+# # for n in range(len(population)):
+# #     visualize_solo_network(population[n], f"reconstructed_{n}")
+# loss = evaluate_population(population, samples, 1, False)
+# print("Loss: ", loss)
+#
+# check = 4
