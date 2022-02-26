0f98dc540c520a99af917be8356d0c9bcf799415
Avery <averyzanegriffin@gmail.com>
2022-02-25 19:27:36 -0500

    Added script which will be the new home of any function used for saving data

---
+++ kashtan-alon/data_save.py
@@ -0,0 +1,36 @@
+"""
+Functions for saving various aspects of the experiement.
+E.g. saving weights, plots, graphs
+"""
+
+import csv
+import time
+
+import json
+from os import makedirs
+from os.path import join
+from matplotlib import pyplot as plt
+import numpy as np
+import pickle
+from tqdm import tqdm
+
+from visualize_nets import write_graphviz, plot_graphviz
+
+
+# def setup_savedir(runname):
+#     makedirs(join("network_graphs", runname).replace("\\", "/"), exist_ok=True)
+#     makedirs("loss_curves", exist_ok=True)
+#     makedirs(f"saved_weights/{runname}", exist_ok=True)
+
+def default(obj):
+    if isinstance(obj, np.ndarray):
+        return obj.tolist()
+    raise TypeError('Not serializable')
+
+
+def save_weights(population, runname, gen):
+    makedirs(f"saved_weights/{runname}/gen_{gen}", exist_ok=True)
+    for i in range(len(population)):
+        w_file = open(f"saved_weights/{runname}/gen_{gen}/network_{i}.json", "w")
+        json.dump(population[i], w_file, default=default)
+        w_file.close()
