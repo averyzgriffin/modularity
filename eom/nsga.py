0017eaf28e5fa300e653490779bb5db3c897723b
avery <averyzanegriffin@gmail.com>
2022-05-26 20:20:02 -0400

    Updated nsga

--- eom/nsga.py
+++ eom/nsga.py
@@ -2,76 +2,95 @@ from collections import defaultdict
 import numpy as np
 import random
 
-N = population_size = 25
-num_parents = population_size*.4
+population_size = 10
+min_value = -10
+max_value = 10
+num_parents = max(int(population_size*.2), 2)
+r = .5
 
 
-def main():
-    population = initialize_pop(N)
-
-    for i in range(100):
-        losses = compute_performance_loss(population)
-        connection_costs = compute_connection_count_costs(population)  # eventually, compute_connection_distance_costs
-        parents = stochastic_dominant_selection(population, r, num_parents)  # use crowding_distance() to break ties
-        offspring = crossover(parents, N)
-        offspring = mutate(offspring)
-        population = population + offspring
-        population = nondomimant_sorting(population)  # use crowding_distance() to sort any overflowing fronts
+# def main():
+#     population = initialize_pop(population_size)
+#
+#     for i in range(100):
+#         losses = compute_performance_loss(population)
+#         connection_costs = compute_connection_count_costs(population)  # eventually, compute_connection_distance_costs
+#         parents = stochastic_dominant_selection(population, r, num_parents)  # use crowding_distance() to break ties
+#         offspring = crossover(parents, N)
+        # offspring = mutate(offspring)
+        # population = population + offspring
+        # population = nondomimant_sorting(population)  # use crowding_distance() to sort any overflowing fronts
 
 
 def main2():
-    population = initialize_pop(100)
-    for i in range(1):
-        orgs = [tuple([obj_1(i), obj_2(i)]) for i in population]
-        # parents = stochastic_dominant_selection(population, r, num_parents)
-        # offspring = crossover(parents, N)
-        # population = population + mutate(offspring)
-        fronts = sortNondominated(orgs)
+    population = initialize_pop(population_size)
+    for i in range(100):
+        pop_objs = [tuple([obj_1(i), obj_2(i)]) for i in population]
+        distances = CrowdingDist(pop_objs)
+
+        parents = stochastic_dominant_selection(population, pop_objs, r, num_parents, distances)
+        offspring = crossover_and_mutate(parents, population_size)
+        offspring_objs = [tuple([obj_1(i), obj_2(i)]) for i in offspring]
+        offspring_distances = CrowdingDist(offspring_objs)
+
+        merged_pop = population + offspring
+        merged_objs = pop_objs + offspring_objs
+        merged_distances = distances + offspring_distances
+
+        fronts = sortNondominated(merged_objs)
+        population = selectPopulationFromFronts(fronts, merged_pop, merged_distances)
+
+        min_loss = min(merged_objs[0])
+        print(i)
+        print(population)
+        print(pop_objs)
 
 
 def initialize_pop(pop_size):
-    x = random.sample(range(-100, 100), pop_size)
+    x = random.sample(range(min_value, max_value), pop_size)
     # y = [{"id": str(i), "genome": j, "obj1": None, "obj2": None} for i,j in zip(range(len(x)), x)]
     return x
 
 
-def stochastic_dominant_selection(population, r, num_parents):
-    parents = []
-    while len(parents) < num_parents:
-        ps = random.sample(range(len(population)), 2)
-        parent1 = population[ps[0]]
-        parent2 = population[ps[1]]
-        current_parents = [parent1, parent2]
-
-        # if (np.random.uniform(0,1) > r and outperforms(parent1[0], parent2[0])) or dominates(parent1["objs"], parent2["objs"], [-1, 1]):
-        #     parents.append(parent1)
-        # elif ( np.random.uniform(0,1) > r and outperforms(parent2[0], parent1[0]) ) or dominates(parent2["objs"], parent1["objs"], [-1, 1]):
-        #     parents.append(parent2)
+def stochastic_dominant_selection(population, pop_objs, r, num_parents, distances):
+    parents_idxs = []
+    while len(parents_idxs) < num_parents:
+        ps = random.sample(range(len(pop_objs)), 2)
+        parent1 = pop_objs[ps[0]]
+        parent2 = pop_objs[ps[1]]
 
         stochastic = np.random.uniform(0, 1) <= r
         if not stochastic:
             if outperforms(parent1[0], parent2[0]):
-                parents.append(parent1)
+                parents_idxs.append(ps[0])
             elif outperforms(parent2[0], parent1[0]):
-                parents.append(parent2)
+                parents_idxs.append(ps[1])
             else:
-                parents.append(current_parents[random.randint(0,1)])
-                # parents.append([parent1, parent2][less_crowded(parent1, parent2)])
+                parents_idxs.append(less_crowded(distances, ps[0], ps[1]))
         elif stochastic:
             if dominates(parent1, parent2, [-1, -1]):
-                parents.append(parent1)
+                parents_idxs.append(ps[0])
             elif dominates(parent2, parent1, [-1, -1]):
-                parents.append(parent2)
+                parents_idxs.append(ps[1])
             else:
-                parents.append(current_parents[random.randint(0,1)])
-                # parents.append([parent1, parent2][less_crowded(parent1, parent2)])
+                parents_idxs.append(less_crowded(distances, ps[0], ps[1]))
+                # if distances[ps[0]] > distances[ps[1]]:
+                #     parents_idxs.append(ps[0])
+                # elif distances[ps[0]] < distances[ps[1]]:
+                #     parents_idxs.append(ps[1])
+                # else:
+                #     parents_idxs.append(current_parents[random.randint(0, 1)][0])
 
-    return parents
+    return [population[i] for i in parents_idxs]
 
 
-def less_crowded(a, b):
-    # TODO Write this function
-    return 1
+def less_crowded(distances, a, b):
+    if distances[a] > distances[b]:
+        return a
+    elif distances[a] < distances[b]:
+        return b
+    else:
+        return random.choice([a, b])
 
 
 def outperforms(a, b, sign=-1):
@@ -103,8 +122,40 @@ def obj_2(x):
     return y
 
 
-def nondomimant_sorting():
-    pass
+def CrowdingDist(fitness=None):
+    """
+    :param fitness: A list of fitness values
+    :return: A list of crowding distances of chrmosomes
+
+    The crowding-distance computation requires sorting the population according to each objective function value
+    in ascending order of magnitude. Thereafter, for each objective function, the boundary solutions (solutions with smallest and largest function values)
+    are assigned an infinite distance value. All other intermediate solutions are assigned a distance value equal to
+    the absolute normalized difference in the function values of two adjacent solutions.
+    """
+
+    # initialize list: [0.0, 0.0, 0.0, ...]
+    distances = [0.0] * len(fitness)
+    crowd = [(f_value, i) for i, f_value in enumerate(fitness)]  # create keys for fitness values
+
+    n_obj = len(fitness[0])
+
+    for i in range(n_obj):  # calculate for each objective
+        crowd.sort(key=lambda element: element[0][i])
+        # After sorting,  boundary solutions are assigned Inf
+        # crowd: [([obj_1, obj_2, ...], i_0), ([obj_1, obj_2, ...], i_1), ...]
+        distances[crowd[0][1]] = float("Inf")
+        distances[crowd[-1][1]] = float("inf")
+        if crowd[-1][0][i] == crowd[0][0][i]:  # If objective values are same, skip this loop
+            continue
+        # normalization (max - min) as Denominator
+        norm = float(crowd[-1][0][i] - crowd[0][0][i])
+        # crowd: [([obj_1, obj_2, ...], i_0), ([obj_1, obj_2, ...], i_1), ...]
+        # calculate each individual's Crowding Distance of i th objective
+        # technique: shift the list and zip
+        for prev, cur, next in zip(crowd[:-2], crowd[1:-1], crowd[2:]):
+            distances[cur[1]] += (next[0][i] - prev[0][i]) / norm  # sum up the distance of ith individual along each of the objectives
+
+    return distances
 
 
 def sortNondominated(individuals, k=None, first_front_only=False):
@@ -178,26 +229,45 @@ def sortNondominated(individuals, k=None, first_front_only=False):
     return fronts
 
 
-def crossover(a,b):
-    r=random.random()
-    if r>0.5:
-        return mutate((a+b)/2)
-    else:
-        return mutate((a-b)/2)
+def selectPopulationFromFronts(fronts, population, distances):
+    idxs = []
+    for front in fronts:
+        if len(idxs) + len(front) <= population_size:
+           idxs.extend(front)
+        else:
+            d = population_size - len(idxs)
+            temp = []
+            for id in front:
+                temp.append((distances[id], id))
+            temp.sort(reverse=True)
+            front = [thing[1] for thing in temp]
+            idxs.extend(front[:d])
+        if len(idxs) == population_size:
+            break
+
+    return [population[i] for i in idxs]
+
+
+def crossover_and_mutate(parents, population_size):
+    offspring = []
+    while len(offspring) < population_size:
+        ps = random.sample(range(len(parents)), 2)
+        p1 = parents[ps[0]]
+        p2 = parents[ps[1]]
+
+        r=random.random()
+        if r>0.5:
+            offspring.append(int(mutate((p1+p2)/2)))
+        else:
+            offspring.append(int(mutate((p1-p2)/2)))
+    return offspring
 
 
 def mutate(solution):
     mutation_prob = random.random()
-    if mutation_prob < 1:
-        solution = -100+(100+100)*random.random()
+    if mutation_prob < .1:
+        solution = random.randint(-10,10)
     return solution
-
-
-def compute_performance_loss():
-    pass
-
-def compute_connection_count_costs():
-    pass
 
 
 main2()
